d936a7ee30178b4ad762e8a92d96dbf4cb562400
Rogelio Arriaga <r.arriaga@generaltransmissions.com>
2024-12-06 14:05:48 -0600

    Update table patientsFiles

--- Medical.Office.SqlLocalDB/ConfigurationTables/MedicalOffice/PatientsFiles.sql
+++ Medical.Office.SqlLocalDB/ConfigurationTables/MedicalOffice/PatientsFiles.sql
@@ -1,14 +1,14 @@
 ﻿CREATE TABLE [dbo].[PatientsFiles]
 (
-	[Id] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
-    [IDPatient] BIGINT NULL,
-    [FileName] VARCHAR(50) NULL, 
-    [FileType] VARCHAR(50) NULL, 
-    [FileExtension] NCHAR(10) NULL, 
-    [ChunkIndex] INT NULL, 
-    [TotalChunks] INT NULL, 
-    [Description] NCHAR(10) NULL, 
-    [FileData] VARBINARY(MAX) NULL,
-    [DateTimeSnap] DATETIME NULL DEFAULT GETDATE(), 
+    [Id] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
+    [IDPatient] BIGINT NOT NULL, -- Asegúrate de que siempre tenga un ID de paciente
+    [FileName] NVARCHAR(Max) NOT NULL, -- Ampliar para nombres de archivo más largos
+    [FileType] NVARCHAR(Max) NOT NULL, -- Tipo de archivo siempre debe estar definido
+    [FileExtension] NVARCHAR(10) NOT NULL, -- Extensión siempre requerida
+    [ChunkIndex] INT NOT NULL, 
+    [TotalChunks] INT NOT NULL, 
+    [Description] NVARCHAR(Max) NULL, -- Ampliar descripción
+    [FileData] VARBINARY(MAX) NOT NULL, -- Los datos del archivo son obligatorios
+    [DateTimeSnap] DATETIME NOT NULL DEFAULT GETDATE(), 
     CONSTRAINT [FK_PatientsFiles_ToPatientData] FOREIGN KEY ([IDPatient]) REFERENCES [PatientData]([ID])
-)
+);
