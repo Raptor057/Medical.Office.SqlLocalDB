60484469824254f65c6f309b07dd33a99f77093b
Rogelio Arriaga <r.arriaga@generaltransmissions.com>
2024-12-24 18:43:03 -0600

    Add utc configuration

---
+++ Medical.Office.SqlLocalDB/UtcTime/UfnToUniversalTime.sql
@@ -0,0 +1,7 @@
+﻿CREATE FUNCTION [dbo].[UfnToUniversalTime](@utcTime DATETIME)
+RETURNS DATETIME
+AS BEGIN
+    DECLARE @localTime AS DATETIME;
+    SELECT @localTime = CAST(@utcTime AT TIME ZONE TimeZone AT TIME ZONE 'UTC' AS DATETIME) FROM GlobalSettings;
+    RETURN @localTime;
+END
