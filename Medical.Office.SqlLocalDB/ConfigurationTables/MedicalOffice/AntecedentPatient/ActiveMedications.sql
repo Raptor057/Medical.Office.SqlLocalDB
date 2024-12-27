050f063533e86d8046d5227459bc98a2a4190e41
Rogelio Arriaga Gonzalez <rogelioarriaga@live.com.mx>
2024-12-27 00:49:55 -0600

    Update

--- Medical.Office.SqlLocalDB/ConfigurationTables/MedicalOffice/AntecedentPatient/ActiveMedications.sql
+++ Medical.Office.SqlLocalDB/ConfigurationTables/MedicalOffice/AntecedentPatient/ActiveMedications.sql
@@ -2,7 +2,7 @@
 (
 	[Id] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
     [IDPatient] BIGINT NOT NULL, 
-    [AactiveMedicationsData] VARCHAR(MAX) NULL, 
+    [ActiveMedicationsData] VARCHAR(MAX) NULL, 
     [DateTimeSnap] DATETIME NULL DEFAULT GetDate(), 
     CONSTRAINT [FK_ActiveMedications_PatientData] FOREIGN KEY ([IDPatient]) REFERENCES [PatientData]([ID])
 )
